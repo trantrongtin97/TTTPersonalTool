dbf13a3b4529106e02bd55bbf0e64872bfc4e0ba
tin <tint@thegenius.com>
2023-12-08 18:12:32 +0700

    Working Automation table

--- TTT.PersonalTool/Shared/Dtos/ItemDto.cs
+++ TTT.PersonalTool/Shared/Dtos/ItemDto.cs
@@ -6,27 +6,26 @@ namespace TTT.PersonalTool.Shared.Dtos;
 public class ItemDto
 {
     public int Id { get; set; }
-    [Required]
+
     [Display(Name = "Name")]
-    [TTTDataType(TTTDataType.Text)]
+    [TTTDataType(TTTDataType.MultilineText)]
     [MaxLength(DefineFieldValue.String_Lenght_100)]
     public string Name { get; set; }
-    [Required]
+
     [Display(Name = "Price")]
     [TTTDataType(TTTDataType.Double)]
-    [Range(1, 100)]
     public double? Price { get; set; }
-    [Required]
+
     [Display(Name = "CreateDate")]
-    [TTTDataType(TTTDataType.Date)]
-    public DateTime? CreateDate { get; set; }
+    [TTTDataType(TTTDataType.Time)]
+    public DateTime CreateDate { get; set; }
 
     [TTTDataType(TTTDataType.Combobox)]
     [Display(Name = "TenantID")]
-    public int? TenantID { get; set; } = 2;
+    public int TenantID { get; set; } = 2;
 
     [DataLookUp("TenantID")]
-    public List<TenantLookUp>? TenantLookUp { get; set; } = new List<TenantLookUp>()
+    public List<TenantLookUp> TenantLookUp { get; set; } = new List<TenantLookUp>()
     {
         new TenantLookUp()
         {
